42c002b1148eef4606cbf3517ca0599c7aaee7c9
qimodau <spotikqimodau@gmail.com>
2026-06-16 17:44:28 +0300

    Update rnnoise-processor.js

--- rnnoise-processor.js
+++ rnnoise-processor.js
@@ -1,41 +1,43 @@
 // rnnoise-processor.js – AudioWorklet‑процессор для шумоподавления
-import createRNNWasmModule from './rnnoise.js';
+const RNNOISE_SAMPLE_LENGTH = 480;
 
 class RnnoiseProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
     this._ready = false;
+    this._context = null;
+    this._wasm = null;
+    this._buffer = new Float32Array(RNNOISE_SAMPLE_LENGTH);
     this._init();
   }
 
   async _init() {
     try {
+      const createRNNWasmModule = (await import('./rnnoise.js')).default;
       const wasm = await createRNNWasmModule();
       this._wasm = wasm;
       this._rnnoise_create = wasm._rnnoise_create;
       this._rnnoise_process_frame = wasm._rnnoise_process_frame;
       this._rnnoise_destroy = wasm._rnnoise_destroy;
       this._context = this._rnnoise_create();
-      this._buffer = new Float32Array(480);
       this._ready = true;
     } catch (e) {
-      console.error('Failed to init RNNoise', e);
+      console.error('Failed to init RNNoise in AudioWorklet', e);
     }
   }
 
   process(inputs, outputs) {
-    if (!this._ready) return true;
-    const input = inputs[0];
-    const output = outputs[0];
-    if (!input || !input[0]) return true;
+    if (!this._ready || !inputs[0] || !inputs[0][0]) return true;
+    const input = inputs[0][0];
+    const output = outputs[0][0];
+    if (input.length === 0) return true;
 
-    for (let i = 0; i < input[0].length; i += 480) {
-      const frame = input[0].subarray(i, i + 480);
-      if (frame.length < 480) break;
+    for (let i = 0; i < input.length; i += RNNOISE_SAMPLE_LENGTH) {
+      const frame = input.subarray(i, i + RNNOISE_SAMPLE_LENGTH);
+      if (frame.length < RNNOISE_SAMPLE_LENGTH) break;
       this._buffer.set(frame);
       this._rnnoise_process_frame(this._context, this._buffer, this._buffer);
-      const outFrame = output[0].subarray(i, i + 480);
-      outFrame.set(this._buffer);
+      output.set(this._buffer, i);
     }
     return true;
   }
