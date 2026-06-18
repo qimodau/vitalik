a1d78fc374f93667418aef9b4ea85115b936c7b2
qimodau <spotikqimodau@gmail.com>
2026-06-18 22:09:41 +0300

    Update rnnoise-processor.js

--- rnnoise-processor.js
+++ rnnoise-processor.js
@@ -9,65 +9,41 @@ class RNNoiseProcessor extends AudioWorkletProcessor {
   constructor(options) {
     super();
     this._ready = false;
+    this._state = null;
     this._module = null;
     this._inputBuf = [];
     this._outputBuf = [];
     this._inputPtr = null;
     this._outputPtr = null;
 
-    const { wasmBinary } = (options && options.processorOptions) || {};
-    if (!wasmBinary) {
-      console.error('[RNNoise processor] wasmBinary не передан');
+    const { wasmBinary, scriptBase } = (options && options.processorOptions) || {};
+    if (!wasmBinary || !scriptBase) {
+      console.error('[RNNoise processor] wasmBinary или scriptBase не переданы');
       return;
     }
 
-    // Импорты, которые ожидает скомпилированный WASM (соответствуют asmLibraryArg из rnnoise.js)
-    const importObject = {
-      a: {
-        // _emscripten_resize_heap
-        a: (requestedSize) => {
-          const oldSize = this._module.memory.buffer.byteLength;
-          requestedSize = requestedSize >>> 0;
-          const maxHeapSize = 2147483648; // 2 ГБ
-          if (requestedSize > maxHeapSize) return 0;
-          const alignUp = (x, multiple) => x + (multiple - x % multiple) % multiple;
-          for (let cutDown = 1; cutDown <= 4; cutDown *= 2) {
-            let overGrownHeapSize = oldSize * (1 + 0.2 / cutDown);
-            overGrownHeapSize = Math.min(overGrownHeapSize, requestedSize + 100663296);
-            let newSize = Math.min(maxHeapSize, alignUp(Math.max(requestedSize, overGrownHeapSize), 65536));
-            let pages = Math.ceil((newSize - oldSize) / 65536);
-            if (pages > 0) {
-              try {
-                this._module.memory.grow(pages);
-                return 1;
-              } catch (e) {}
-            }
-          }
-          return 0;
-        },
-        // _emscripten_memcpy_big
-        b: (dest, src, num) => {
-          const heapU8 = new Uint8Array(this._module.memory.buffer);
-          heapU8.copyWithin(dest, src, src + num);
-        }
+    try {
+      // Синхронно загружаем rnnoise.js (importScripts работает в AudioWorklet)
+      importScripts(scriptBase + 'rnnoise.js');
+      const createRNNWasmModule = self.createRNNWasmModule;
+      if (typeof createRNNWasmModule !== 'function') {
+        throw new Error('createRNNWasmModule не определён после importScripts');
       }
-    };
-
-    // Загружаем WASM с правильными импортами
-    WebAssembly.instantiate(wasmBinary, importObject)
-      .then((result) => {
-        const exports = result.instance.exports;
-        this._module = exports;
-        this._inputPtr = exports._malloc(FRAME * 4);
-        this._outputPtr = exports._malloc(FRAME * 4);
-        this._state = exports._rnnoise_create(0);
+      createRNNWasmModule({ wasmBinary }).then((mod) => {
+        this._module = mod;
+        this._inputPtr = mod._malloc(FRAME * 4);
+        this._outputPtr = mod._malloc(FRAME * 4);
+        this._state = mod._rnnoise_create(0);
         this._ready = true;
-        console.log('[RNNoise processor] WASM загружен и готов');
-      })
-      .catch((e) => {
-        console.error('[RNNoise processor] ошибка инициализации WASM:', e);
+        console.log('[RNNoise processor] RNNoise загружен и готов');
+      }).catch((e) => {
+        console.error('[RNNoise processor] ошибка инициализации:', e);
         this._ready = false;
       });
+    } catch (e) {
+      console.error('[RNNoise processor] ошибка загрузки rnnoise.js:', e);
+      this._ready = false;
+    }
   }
 
   _denoise(inputF32) {
