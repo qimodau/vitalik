e0b01fa586e577e7e27b9629d279f2127b470aa5
qimodau <spotikqimodau@gmail.com>
2026-06-18 12:29:43 +0300

    Update rnnoise-processor.js

--- rnnoise-processor.js
+++ rnnoise-processor.js
@@ -29,25 +29,36 @@ class RNNoiseProcessor extends AudioWorkletProcessor {
 
   async _initWasm(wasmBuffer) {
     try {
-      const mem = new WebAssembly.Memory({ initial: 256, maximum: 512 });
-      const { instance } = await WebAssembly.instantiate(wasmBuffer, {
+      // Полный набор импортов, которые ожидает RNNoise (Emscripten)
+      const importObj = {
         env: {
-          memory: mem,
+          memory: new WebAssembly.Memory({ initial: 256, maximum: 512 }),
           table: new WebAssembly.Table({ initial: 0, element: 'anyfunc' }),
           abort: () => {},
           __assert_fail: () => {},
-          emscripten_resize_heap: () => 0,
-        },
-        wasi_snapshot_preview1: { proc_exit: () => {} }
-      });
+          emscripten_resize_heap: (size) => 0,
+          emscripten_notify_memory_growth: () => {},
+          getTempRet0: () => 0,
+          setTempRet0: (val) => {},
+          // Дополнительные функции, которые могут понадобиться
+          _emscripten_get_now: () => performance.now(),
+          _emscripten_throw_type_error: (msg) => { throw new Error(msg); },
+          _emscripten_throw_range_error: (msg) => { throw new Error(msg); },
+          // Для WASI (может не использоваться, но оставляем)
+          wasi_snapshot_preview1: {
+            proc_exit: () => {}
+          }
+        }
+      };
 
+      const { instance } = await WebAssembly.instantiate(wasmBuffer, importObj);
       this._exp = instance.exports;
-      this._st     = this._exp.rnnoise_create(0);
-      this._inPtr  = this._exp.malloc(FRAME * 4);
+      this._st = this._exp.rnnoise_create(0);
+      this._inPtr = this._exp.malloc(FRAME * 4);
       this._outPtr = this._exp.malloc(FRAME * 4);
-      this._ready  = true;
+      this._ready = true;
       this.port.postMessage({ type: 'ready', success: true });
-    } catch(e) {
+    } catch (e) {
       console.error('[RNNoise processor] init error:', e);
       this.port.postMessage({ type: 'ready', success: false, error: String(e) });
     }
@@ -55,7 +66,7 @@ class RNNoiseProcessor extends AudioWorkletProcessor {
 
   _denoise(inputF32) {
     const heap = new Float32Array(this._exp.memory.buffer);
-    const inOff  = this._inPtr  / 4;
+    const inOff  = this._inPtr / 4;
     const outOff = this._outPtr / 4;
 
     // RNNoise хочет float в диапазоне int16 (-32768..32768)
